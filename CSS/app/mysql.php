1afd30face5def7fecf3e90d9895124e58155efd
Ernesto2602 <mkasinaloa@gmail.com>
2022-03-29 08:14:49 -0700

    C13
    Definir la clase MySql Usuario1

--- CSS/app/mysql.php
+++ CSS/app/mysql.php
@@ -1,1 +1,11 @@
 <?php
+
+use \Mysqi;
+class Mysql extends \Mysqli{
+    protected $data ={
+        'host' => '127.0.0.1',
+        'user' => 'root',
+        'password' =>'',
+        'port' => 3306
+    }
+}
